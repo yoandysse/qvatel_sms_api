ad6e2a4f54c004d3be12736333545b8db794f8e9
Yoandy Isse Oña <yoandysse@gmail.com>
2023-06-18 01:45:03 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
     version='1.0',
     packages=['qvatel'],
     url='',
-    license='',
+    license='MIT',
     author='Yoandy Isse',
     author_email='yoandysse@gmail.com',
     description='Una librería de Python para enviar SMS a través de QvaTel.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['sms', 'qvatel', 'api', 'python', 'enviar sms', 'sms masivos', 'sms marketing', 'sms api', 'sms gateway'],
     install_requires=[
         'requests>=2.31.0,<3.0.0'
     ]
 )
-
